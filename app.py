430abaf43734fd9a1c62031f0d85d7bf281d24e1
pythondev23 <abbc80941@gmail.com>
2023-09-04 13:59:10 +0200

    final version

--- app.py
+++ app.py
@@ -29,22 +29,31 @@ def check_hex(hex_code: str) -> bool:
     # alternative für - erlaubte zeichen: 0-9 und A-F / a-f
     #if len(set(hex_code).difference(set("0123456789ABCDEF"))) == 0:
     #    return True
-   
-    
-st.write("Valide?", check_hex(hex_in))
 
+def convert_hex_to_rgb(hex_code: str) -> int:   
+    hex = "0123456789ABCDEF"
+    return (hex.index(hex_code[0])*16) + (hex.index(hex_code[1]))
+    
 # 2) umwandeln:
+if check_hex(hex_in):
 
-col_R, col_G, col_B = st.columns(3)
+    r = convert_hex_to_rgb(hex_in[1:3])
+    g = convert_hex_to_rgb(hex_in[3:5])
+    b = convert_hex_to_rgb(hex_in[5:7])
 
-with col_R:
-    st.subheader("Rot:")
-    st.write("...")
+    col_R, col_G, col_B = st.columns(3)
 
-with col_G:
-    st.subheader("Grün:")
-    st.write("...")
-    
-with col_B:
-    st.subheader("Blau:")
-    st.write("...")
+    with col_R:
+        st.subheader("Rot:")
+        st.subheader(r)
+
+    with col_G:
+        st.subheader("Grün:")
+        st.subheader(g)
+        
+    with col_B:
+        st.subheader("Blau:")
+        st.subheader(b)
+        
+else:
+    st.error("Das ist kein valider Hex-Code!")
